a84814d0a591c611f51da442dc44daadf1d4f280
howolcat <pbsokok7@gmail.com>
2026-09-17 14:15:05 +0900

    Add files via upload

---
+++ 실습01.js
@@ -0,0 +1,29 @@
+function f01(){
+  c = document.querySelector("#color01").value;
+  x = document.querySelector("#target01")
+  console.log(x);
+  x.style.backgroundColor = c;
+}
+
+function f02(num){
+  x = document.querySelector("#target02");
+  x.style.width = num + 'px';
+  x.style.height = num + 'px';
+}
+
+function f03(){
+  input31 = document.querySelector("#input31").value;
+  input32 = document.querySelector("#input32").value;
+  input33 = document.querySelector("#input33").value;
+  resultArea = document.querySelector("#result-area");
+
+  s = "구매자 : "+ input31 +", 상품명"+ input32 +", 가격 : "+ input33;
+  resultArea.innerHTML = s;
+
+  console.log(input31);
+  console.log(input32);
+  console.log(input33);
+}
+
+
+
